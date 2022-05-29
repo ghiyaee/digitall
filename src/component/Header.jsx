87d24fc041dd5b96fdd9527028c7203f907c4c5b
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-29 12:26:16 +0430

    cearte digi Mobile

---
+++ src/component/Header.jsx
@@ -0,0 +1,17 @@
+import './header.css'
+const Header = () => {
+    return (
+        <>
+            <div className="header__container">
+                <div className="header__logo">
+                    
+                    <h2></h2>
+                </div>
+                <div className="header__searchbar">search</div>
+                <div className="navbar">navbar</div>
+           </div>
+        </>
+      );
+}
+ 
+export default Header;
