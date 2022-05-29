d012092b74d1dc0dec5b692d9ae168f97544618a
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-29 15:48:23 +0430

    header ok

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,37 @@
-import './header.css'
+import "./header.css";
 const Header = () => {
-    return (
-        <>
-            <div className="header__container">
-                <div className="header__logo">
-                    
-                    <h2></h2>
-                </div>
-                <div className="header__searchbar">search</div>
-                <div className="navbar">navbar</div>
-           </div>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <div className="header__container">
+        <div className="header__logo">
+          <i className="mobile big icon" />
+          <h2 className="header__text">Digi Shop</h2>
+        </div>
+
+        <div className="header__searchbar">
+          <input type="text" className="header__search" />
+          <i className="search icon big  icon__searchbar"></i>
+        </div>
+
+        <div className="navbar">
+          <div className="nav__item">
+            <span className="nav__itemLineOne">Hello User</span>
+            <span className="nav__item__Sign">Sign In</span>
+          </div>
+          <div className="nav__item">
+            <span className="nav__itemLineOne">Your</span>
+            <span className="nav__item__shop">Shop</span>
+          </div>
+          <div className="nav__item">
+            <span className="nav__itemLineOne">
+              <i className="shopping cart big icon"></i>
+            </span>
+            <span className="nav__itemLineTwo">10</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Header;
