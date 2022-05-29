89dde4fa067a67290a1295ad2cdcddd4a2df60e1
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-29 18:11:42 +0430

    add products

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,10 +1,22 @@
 import "./home.css"
 import img from "../assets/imagebk.png";
+import Products from "./Products";
 const Home = () => {
     return (
       <>
         <div className="home__container">
-          <img src={img} alt="" />
+          <img src={img} alt="" className="home__image"/>
+          <div className="product__row">
+            <Products />
+            <Products />
+          </div>
+          <div className="product__row">
+            <Products />
+            <Products />
+             <Products/>
+          </div>
+          <div className="product__row"></div>
+          <Products/>
         </div>
       </>
     );
--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,8 +1,22 @@
+import img from "../assets/iphone-13-pro.jpg";
+import "./products.css"
 const Products = () => {
-    return (  
-        <>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <div className="product__container">
+        <img src={img} alt="" />
+        <div className="product__info">
+          <p>titel</p>
+          <p>price</p>
+          <i className="star yellow icon"></i>
+          <i className="star yellow icon"></i>
+          <i className="star yellow icon"></i>
+          <i className="star yellow icon"></i>
+        </div>
+        <button className="">Buy</button>
+      </div>
+    </>
+  );
+};
+
 export default Products;
