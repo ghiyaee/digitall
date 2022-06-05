f7531d529f40b58007d044a45ea7173a3d8c6e20
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-06-05 17:48:46 +0430

    update style login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,5 +1,10 @@
+import { useState ,useRef} from "react";
 import "./login.css";
 const Login = () => {
+    const ref = useRef();
+    const [email, setImail] = useState('');
+    const [password,setPassword]=useState('')
+
   return (
     <>
       <div className="login__container">
@@ -8,8 +13,8 @@ const Login = () => {
           <h2 className="header__text">Digi Shop</h2>
         </div>
         <div className="login__sing">
-          <input type="text" name="" id="" />
-          <input type="text" name="" id="" />
+          <input type="text" name="" id="" placeholder="E-mail"/>
+          <input type="text" name="" id="" placeholder="PassWord"/>
           <button className="login__btn__sin">Sign In</button>
           <p>By Useing && Buying Please a Create Account a Short Time</p>
           <button className="login__btn__crt">Create Account</button>
