a5d2cb18a310cf6edb83590de30263bade604762
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-06-05 18:21:33 +0430

    fix inputs login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,7 @@
 import { useState ,useRef} from "react";
 import "./login.css";
 const Login = () => {
-    const ref = useRef();
+    const onRef = useRef();
     const [email, setImail] = useState('');
     const [password,setPassword]=useState('')
 
@@ -13,9 +13,9 @@ const Login = () => {
           <h2 className="header__text">Digi Shop</h2>
         </div>
         <div className="login__sing">
-          <input type="text" name="" id="" placeholder="E-mail"/>
-          <input type="text" name="" id="" placeholder="PassWord"/>
-          <button className="login__btn__sin">Sign In</button>
+          <input type="text" name="" id="" placeholder="E-mail" onChange={e=>{setImail(e.target.value)}} value={email} />
+          <input type="text" name="" id="" placeholder="PassWord" onChange={e=>{setPassword(e.target.value)}} value={password}/>
+          <button className="login__btn__sin" onClick={()=>console.log(email,password)}>Sign In</button>
           <p>By Useing && Buying Please a Create Account a Short Time</p>
           <button className="login__btn__crt">Create Account</button>
         </div>
